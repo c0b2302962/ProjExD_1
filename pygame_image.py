7f12d1223040f4ed47dd3a8b4af4724929218774
C0B23029 <c0b2302962@edu.teu.ac.jp>
2024-09-24 15:43:31 +0900

    練習５FPS変更

--- pygame_image.py
+++ pygame_image.py
@@ -22,7 +22,7 @@ def main():
         screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
-        clock.tick(200)
+        clock.tick(200)#aaS
 
 
 if __name__ == "__main__":
