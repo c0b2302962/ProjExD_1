eaa950506b2488bfdeee9ef0af37d1dc044a6409
C0B23029 <c0b2302962@edu.teu.ac.jp>
2024-09-24 17:11:51 +0900

    演習課題1

--- pygame_image.py
+++ pygame_image.py
@@ -26,16 +26,19 @@ def main():
         if key_lst[pg.K_DOWN]:
             kk_rct.move_ip((0, +1))
         if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip((2, 0))
+        else:
             kk_rct.move_ip((-1, 0))
-        if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip((+1, 0))
+        #if key_lst[pg.K_LEFT]:
+        #    kk_rct.move_ip((+1, 0))
 
-        x = -(tmr%800)
+        x = -(tmr%3200)
         screen.blit(bg_img, [x, 0])
         screen.blit(bg_img2, [x+1600, 0])
         screen.blit(bg_img, [x+3200, 0])
         screen.blit(bg_img2, [x+4800, 0]) 
         #screen.blit(kk_img, [300, 200])
+
         screen.blit(kk_img, kk_rct) #8-5
         pg.display.update()
         tmr += 1        
